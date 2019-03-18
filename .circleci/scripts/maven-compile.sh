7c46c8be1d263292c8290fbc4d69ead4c30e3889
Ronny Trommer <ronny@opennms.org>
2019-03-18 17:59:12 +0100

    Fix shellcheck error

--- .circleci/scripts/maven-compile.sh
+++ .circleci/scripts/maven-compile.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Compile checkstyle module"
-cd checkstyle
+cd checkstyle || exit 1
 mvn install -DupdatePolicy=never -DskipTests=true -DskipITs=true -T 1C -Dopennms.home=/opt/opennms -Dinstall.version="${INSTALL_VERSION}" || exit
 cd ..
 
