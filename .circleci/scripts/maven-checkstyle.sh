0d554aac5b137ac76ad9abb70dc5d190f1e5c2d4
Ronny Trommer <ronny@opennms.org>
2019-03-18 21:05:23 +0100

    Add CLI options for maven

--- .circleci/scripts/maven-checkstyle.sh
+++ .circleci/scripts/maven-checkstyle.sh
@@ -2,4 +2,4 @@
 
 echo "Compile checkstyle module"
 cd checkstyle || exit 1
-mvn install -DupdatePolicy=never -DskipTests=true -DskipITs=true -T 1C -Dopennms.home=/opt/opennms -Dinstall.version="${INSTALL_VERSION}" || exit
+mvn install -DupdatePolicy=never -DskipTests=true -DskipITs=true -T 1C -Dopennms.home=/opt/opennms -Dinstall.version="${INSTALL_VERSION}" --batch-mode --errors --fail-at-end --show-version || exit
--- .circleci/scripts/maven-compile.sh
+++ .circleci/scripts/maven-compile.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 echo "Compile whole project"
-mvn install -DupdatePolicy=never -DskipTests=true -DskipITs=true -T 1C -Dopennms.home=/opt/opennms -Dinstall.version="${INSTALL_VERSION}" --batch-mode -Prun-expensive-tasks || exit
-mvn install -DupdatePolicy=never -DskipTests=true -DskipITs=true -T 1C -Dopennms.home=/opt/opennms -Dinstall.version="${INSTALL_VERSION}" --batch-mode -Prun-expensive-tasks -Pdefault --file opennms-full-assembly/pom.xml || exit
-mvn install -DupdatePolicy=never -DskipTests=true -DskipITs=true -T 1C -Dopennms.home=/opt/opennms -Dinstall.version="${INSTALL_VERSION}" --batch-mode -Prun-expensive-tasks --non-recursive --file opennms-tools/pom.xml || exit
-mvn install -DupdatePolicy=never -DskipTests=true -DskipITs=true -T 1C -Dopennms.home=/opt/opennms -Dinstall.version="${INSTALL_VERSION}" --batch-mode -Prun-expensive-tasks --file opennms-tools/centric-troubleticketer/pom.xml || exit
-mvn javadoc:aggregate -DupdatePolicy=never --batch-mode -Prun-expensive-tasks || exit
+mvn install -DupdatePolicy=never -DskipTests=true -DskipITs=true -T 1C -Dopennms.home=/opt/opennms -Dinstall.version="${INSTALL_VERSION}" --batch-mode --errors --show-version -Prun-expensive-tasks || exit
+# mvn install -DupdatePolicy=never -DskipTests=true -DskipITs=true -T 1C -Dopennms.home=/opt/opennms -Dinstall.version="${INSTALL_VERSION}" --batch-mode --errors --show-version -Prun-expensive-tasks -Pdefault --file opennms-full-assembly/pom.xml || exit
+# mvn install -DupdatePolicy=never -DskipTests=true -DskipITs=true -T 1C -Dopennms.home=/opt/opennms -Dinstall.version="${INSTALL_VERSION}" --batch-mode --errors --show-version -Prun-expensive-tasks --non-recursive --file opennms-tools/pom.xml || exit
+# mvn install -DupdatePolicy=never -DskipTests=true -DskipITs=true -T 1C -Dopennms.home=/opt/opennms -Dinstall.version="${INSTALL_VERSION}" --batch-mode --errors --show-version -Prun-expensive-tasks --file opennms-tools/centric-troubleticketer/pom.xml || exit
+# mvn javadoc:aggregate -DupdatePolicy=never --batch-mode -Prun-expensive-tasks || exit
