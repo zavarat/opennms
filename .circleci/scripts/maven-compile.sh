19762b18f0bfdcdc4a646d8274b89a8c92e04ac4
Ronny Trommer <ronny@opennms.org>
2019-03-18 17:05:25 +0100

    Remove variable MAVEN_FLAGS

--- .circleci/scripts/maven-compile.sh
+++ .circleci/scripts/maven-compile.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 echo "Compiling..."
-MAVEN_FLAGS="-DupdatePolicy=never -DskipTests=true -DskipITs=true -T 1C"
-mvn install "${MAVEN_FLAGS} -Dopennms.home=/opt/opennms -Dinstall.version=${INSTALL_VERSION} --batch-mode -Prun-expensive-tasks" || exit
-mvn install "${MAVEN_FLAGS} -Dopennms.home=/opt/opennms -Dinstall.version=${INSTALL_VERSION} --batch-mode -Prun-expensive-tasks -Pdefault --file opennms-full-assembly/pom.xml" || exit
-mvn install "${MAVEN_FLAGS} -Dopennms.home=/opt/opennms -Dinstall.version=${INSTALL_VERSION} --batch-mode -Prun-expensive-tasks --non-recursive --file opennms-tools/pom.xml" || exit
-mvn install "${MAVEN_FLAGS} -Dopennms.home=/opt/opennms -Dinstall.version=${INSTALL_VERSION} --batch-mode -Prun-expensive-tasks --file opennms-tools/centric-troubleticketer/pom.xml" || exit
+mvn -X install -DupdatePolicy=never -DskipTests=true -DskipITs=true -T 1C -Dopennms.home=/opt/opennms -Dinstall.version="${INSTALL_VERSION}" --batch-mode -Prun-expensive-tasks || exit
+mvn -X install -DupdatePolicy=never -DskipTests=true -DskipITs=true -T 1C -Dopennms.home=/opt/opennms -Dinstall.version="${INSTALL_VERSION}" --batch-mode -Prun-expensive-tasks -Pdefault --file opennms-full-assembly/pom.xml || exit
+mvn -X install -DupdatePolicy=never -DskipTests=true -DskipITs=true -T 1C -Dopennms.home=/opt/opennms -Dinstall.version="${INSTALL_VERSION}" --batch-mode -Prun-expensive-tasks --non-recursive --file opennms-tools/pom.xml || exit
+mvn -X install -DupdatePolicy=never -DskipTests=true -DskipITs=true -T 1C -Dopennms.home=/opt/opennms -Dinstall.version="${INSTALL_VERSION}" --batch-mode -Prun-expensive-tasks --file opennms-tools/centric-troubleticketer/pom.xml || exit
 mvn javadoc:aggregate -DupdatePolicy=never --batch-mode -Prun-expensive-tasks || exit
