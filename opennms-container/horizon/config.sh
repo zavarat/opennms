bbdbe76260f1ca7efab1ee3bd8d770f506fc5061
Ronny Trommer <ronny@opennms.org>
2019-03-31 20:37:07 +0200

    Add build step to build Horizon OCI

---
+++ opennms-container/horizon/config.sh
@@ -0,0 +1,40 @@
+#!/bin/bash -e
+
+# shellcheck disable=SC2034
+
+# Base Image Dependency
+BASE_IMAGE="opennms/openjdk"
+BASE_IMAGE_VERSION="1.8.0.201.b09-b1"
+BUILD_DATE="$(date -u +"%Y-%m-%dT%H:%M:%S%z")"
+
+# Horizon RPM repository config and version
+VERSION="bleeding"
+BUILD_NUMBER="${CIRCLE_BUILD_NUM}"
+IMAGE_VERSION="${VERSION}-${BUILD_NUMBER}"
+
+REPO_HOST="yum.opennms.org"
+REPO_RELEASE="stable"
+REPO_RPM="https://${REPO_HOST}/repofiles/opennms-repo-${REPO_RELEASE}-rhel7.noarch.rpm"
+REPO_KEY_URL="https://${REPO_HOST}/OPENNMS-GPG-KEY"
+
+# System Package dependencies
+PACKAGES="wget
+          gettext"
+
+# OpenNMS Horizon dependencies
+PACKAGES="${PACKAGES}
+          rrdtool
+          jrrd2
+          R-core"
+
+#
+# If you want to install packages from the official repository, add your packages here.
+# By default the build system will build the RPMS in the ./rpms directory and install from here.
+#
+# Suggested packages to install OpenNMS Horizon packages from repository
+#
+# PACKAGES="${PACKAGES}
+#           opennms-core
+#           opennms-webapp-jetty
+#           opennms-plugin-protocol-cifs
+#           opennms-webapp-hawtio"
---
+++ opennms-container/sentinel/config.sh
@@ -0,0 +1,34 @@
+#!/bin/bash -e
+
+# shellcheck disable=SC2034
+
+# Base Image Dependency
+BASE_IMAGE="opennms/openjdk"
+BASE_IMAGE_VERSION="1.8.0.191.b12-b3"
+BUILD_DATE="$(date -u +"%Y-%m-%dT%H:%M:%S%z")"
+
+# Horizon RPM repository config and version
+VERSION="bleeding"
+BUILD_NUMBER="b1"
+IMAGE_VERSION="${VERSION}-${BUILD_NUMBER}"
+
+REPO_HOST="yum.opennms.org"
+REPO_RELEASE="stable"
+REPO_RPM="https://${REPO_HOST}/repofiles/opennms-repo-${REPO_RELEASE}-rhel7.noarch.rpm"
+REPO_KEY_URL="https://${REPO_HOST}/OPENNMS-GPG-KEY"
+
+# System Package dependencies
+PACKAGES="wget
+          gettext"
+
+#
+# If you want to install packages from the official repository, add your packages here.
+# By default the build system will build the RPMS in the ./rpms directory and install from here.
+#
+# Suggested packages to install OpenNMS Minion packages from repository
+# PACKAGES="${PACKAGES}
+#           opennms-sentinel"
+
+# Run as user
+USER="sentinel"
+GROUP="sentinel"
