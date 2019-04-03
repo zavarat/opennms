ec2f88f93b58fdf628ae6389c8ada0e707fca3c6
Ronny Trommer <ronny@opennms.org>
2019-04-03 15:46:11 +0200

    Add publishing for Horizon, Minion and Sentinel to my DockerHub repo no42org for testing

--- opennms-container/sentinel/publish.sh
+++ opennms-container/sentinel/publish.sh
@@ -11,3 +11,7 @@ source ./config.sh
 
 # shellcheck source=opennms-container/registry-config.sh
 source ../registry-config.sh
+
+for TAG in ${IMAGE_VERSION[*]}; do
+  docker push "${CONTAINER_REGISTRY}/${CONTAINER_REGISTRY_REPO}/sentinel:${TAG}"
+done
